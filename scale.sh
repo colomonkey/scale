09dd5d2a1277133d4b564852287843b5ad29dbba
Rosco <rosco@cloudvps.com>
2014-10-13 09:56:41 +0200

    Update scale.sh
    Added more verbose description in comments.

--- scale.sh
+++ scale.sh
@@ -2,14 +2,23 @@
 # Get haproxy queue and call add or del server playbook.
 # Usage: ./<scriptname> <webfarm> <BACKEND>
 
-maxqueu=10 # max queue size
-maxserv=3  # max servers
+# !Todo: No user input validation
+maxqueu=$1 # max queue size
+maxserv=4  # max servers
 currque=0  # current servers
 counter=0  # start with empty counter
 
+webfarm="webfarm"  # Name of haproxy webserver group
+backend="BACKEND"  # Name of haproxy webserver group statisctics row
+
+echo "- Scaling starts when more then $1 requests are waiting in the queue"
+echo "- Scaling to the maximum of 4 servers"
+echo "- When there are no more requests waiting, scale down to minimun of one server"
+echo ""
+
 while true
 do
-    queue=$(curl -u admin:hadmin "http://0.0.0.0:8808/stats/;csv" 2>/dev/null | grep "^$1,$2" |cut -d, -f 3)
+    queue=$(curl -u admin:hadmin "http://213.187.241.159:443/stats/;csv" 2>/dev/null | grep "^$webfarm,$backend" |cut -d, -f 3)
     if [ -z "$queue" ]; then
             echo "ERROR: Please HAproxy fix URL"
             exit 1
@@ -18,24 +27,26 @@ do
                 echo "Max servers exceeded, please raise the limit"
         else
                 currque=$[currque + 1]
-                echo "Queu limit exceeded, spawning extra server"
+                echo "Queu waiting limit exceeded, spawning extra server to handle extra requests"
                 echo "Spawning web-extra-"$currque
-                /usr/local/bin/ansible-playbook -i inventory add_server.yml --extra-vars "servername=web-extra-$currque"
-                sleep 60 # new server warmup
-                counter=$[counter + 30] # 30 points for the new server
+                /usr/local/bin/ansible-playbook -i inventory add_server.yml --extra-vars "servername=web-extra-$currque" > /dev/null 2>&1
+                sleep 15 # new server warmup
+                counter=$[counter + 10] # 30 points for the new server
         fi
     elif [ "$counter" -gt 0 ]; then
                 counter=$[counter - 1]
     elif [ "$counter" == 0 ]; then
         if [ "$currque" -gt 0 ]; then
             server="web-extra-$currque"
-            echo "Scaling down, removing: "$server
-            sed -e /"$server"/d -i inventory # Nasty hack because ansible cannot change the inventory file
+            echo "Looks good, scaling down and removing: "$server
+            sed -e /"$server"/d -i inventory # Nasty hack because ansible cannot change the inventory file, please use dynamic inventory.
             currque=$[currque - 1]
-            /usr/local/bin/ansible-playbook -i inventory del_server.yml --extra-vars "servername="$server
+            /usr/local/bin/ansible-playbook -i inventory del_server.yml --extra-vars "servername="$server > /dev/null 2>& 1
         fi
     fi
-    echo "Everything is fine, queuesize: $queue"
-    echo "Counter: " $counter
+    echo "Everything is fine, currently waiting requests: $queue"
+    if [ ! "$counter" == 0 ]; then
+        echo "Countdown to scale (down): " $counter
+    fi
     sleep 1
 done
